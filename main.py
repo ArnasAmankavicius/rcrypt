a32955d7429acb806955608f5297063e6aff9f8f
Hash <arnas.jonas.a@gmail.com>
2022-03-11 13:25:02 -0600

    cleaning up the code

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import chunk
-from genericpath import isdir
-from threading import get_ident
-from Crypto.Hash import SHA512
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from pathlib import Path
 from os import getcwd, mkdir, path, SEEK_END
 from alive_progress import alive_bar
+from utils import get_file_size, calculate_file_hash
+
+from rcrypt import generate_key, encrypt_file
 
 import argparse
-
-from isort import file
 
 # TODO :
 ## Clean up the code, possible separate it.
@@ -31,64 +29,17 @@ if(dev_mode == 1):
 
 def setup_parser():
     parser = argparse.ArgumentParser(description="figure it out")
-    parser.add_argument('file', metavar='file', type=str)
+    parser.add_argument('-f','--file', type=str)
     parser.add_argument('-a', '--decrypt', action='store_true')
     parser.add_argument('-e', '--encryption-file', type=str)
     parser.add_argument('-d', '--decryption-file', type=str)
     parser.add_argument('-c', '--chunk-size', type=int)
+    parser.add_argument('-g', '--generate-rsa', type=int)
+
     return parser.parse_args()
 
-def generate_key(key_size=2048):
-    print(f"generating rsa key | size {key_size}")
-    keys = RSA.generate(key_size)
-    if not Path(key_path).exists():
-        mkdir(key_path)
-    print("saving private key...")
-    with open(f"{key_path}/key_rsa", "wb") as f:
-        f.write(keys.export_key())
-    print("saving public key...")
-    with open(f"{key_path}/key_rsa.pub", "wb") as f:
-        f.write(keys.public_key().export_key())
-    print('done')
-    return keys
-
-def encrypt_file(file_path: str, rsa_key: str):
-
-    if not Path(file_path).exists():
-        print(f'file path provided does not exist : {file_path}')
-
-    print('getting rsa key info...')
-    key = RSA.import_key(open(rsa_key).read())
-    print('generating random aes key...')
-    session_key = get_random_bytes(16)
-    print('reading file...')
-    with open(file_path, "rb") as f:
-       data = f.read()
-    
-    print(f'generating and encrypting keys')
-    cipher_rsa = PKCS1_OAEP.new(key)
-    enc_session_key = cipher_rsa.encrypt(session_key)
-
-    cipher_aes = AES.new(session_key, AES.MODE_EAX)
-    w_file = open(file_path, 'bw')
-
-    # write aes data
-    print("writing aes information to file...")
-    print(data)
-    cipher_text, tag = cipher_aes.encrypt_and_digest(data)
-    [w_file.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, cipher_text)]
-    
-    print('closing files...')
-    w_file.close()
-
-def get_file_size(handler):
-    handler.seek(0, SEEK_END)
-    file_size = handler.tell()
-    handler.seek(0)
-    return file_size
-
 #pylint:disable=not-callable
-def decrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 8192):
+def decrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 2097152):
     print('getting rsa info...')
     key = RSA.import_key(open(rsa_key).read())
     
@@ -119,36 +70,38 @@ def decrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 8192):
 
     except KeyboardInterrupt:
         print('decryption interrupted')
+    except ValueError as e:
+        print(e)
 
     print('closing files...')
     file_in.close()
     file_out.close()
 
-def encrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 8192):
+# TODO - See if running this on the GPU give any performance boost.
+def encrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 2097152):
     print('getting rsa info...')
     key = RSA.import_key(open(rsa_key).read())
 
     print('generating random aes key...')
     session_key = get_random_bytes(16)
-
-    print('getting file information...')
-    file_in = open(file_path, 'rb')
-    file_size = get_file_size(file_in)
-    
-    print('generating and encrypting keys...')
-    cipher_rsa = PKCS1_OAEP.new(key)
-    enc_session_key = cipher_rsa.encrypt(session_key)
-
-    print('writing aes information to file...')
-    file_out = open(f"{file_path}.enc", 'wb')
-    file_out.write(enc_session_key)
-
     try:
+        print('getting file information...')
+        file_in = open(file_path, 'rb')
+        file_size = get_file_size(file_in)
+        file_hash = calculate_file_hash(file_in)
+        
+        print(f'file hash : {file_hash}')
+        print('generating and encrypting keys...')
+        cipher_rsa = PKCS1_OAEP.new(key)
+        enc_session_key = cipher_rsa.encrypt(session_key)
+ 
+        print('writing aes information to file...')
+        file_out = open(f"{file_path}.enc", 'wb')
+        file_out.write(enc_session_key)
+
         with alive_bar(file_size, title="encrypting...") as bar:
             while(data := file_in.read(chunk_size)):
                 cipher_aes = AES.new(session_key, AES.MODE_EAX)
-                #print(data)
-                #print(f'{file_in.tell()}/{file_size}')
                 ciphertext, tag = cipher_aes.encrypt_and_digest(data)
                 file_out.write(cipher_aes.nonce)
                 file_out.write(tag)
@@ -157,35 +110,27 @@ def encrypt_file_algo(file_path: str, rsa_key: str, chunk_size = 8192):
     except KeyboardInterrupt:
         print("encryption interrupted...")
 
-    print('closing files...')
-    file_in.close()
-    file_out.close()
 
-def decrypt_file(file_path: str, rsa_key: str):
-    print('getting rsa key info...')
-    key = RSA.import_key(open(rsa_key).read())
-    print('extracting info from the file...')
-    file_in = open(file_path, "rb")
-    enc_session_key, nonce, tag, cipher_text = [file_in.read(x) for x in (key.size_in_bytes(), 16, 16, -1)]
+    try:
+        file_out.close()
+        print('file_out handler closed...')
+    except UnboundLocalError:
+        print("file_out was never opened...")
 
-    print('getting session key...')
-    cipher_rsa = PKCS1_OAEP.new(key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
-
-    print('decrypting and verifying data')
-    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-    data = cipher_aes.decrypt_and_verify(cipher_text, tag)
-    with open(file_path, "wb") as f:
-        f.write(data)
-    
-    print('decryption complete')
-    file_in.close()
-
+    try:
+        file_in.close()
+        print('file_in handler closed...')
+    except UnboundLocalError:
+        print("file_in was never opened...")
 
 if __name__ == "__main__":
     args = setup_parser()
     
     decrypt = args.decrypt
+
+    if args.generate_rsa:
+        generate_key(key_path, args.generate_rsa)
+        exit(0)
 
     if decrypt:  
         if Path(args.decryption_file).exists():
@@ -194,6 +139,6 @@ if __name__ == "__main__":
             print(f"did not specify decryption file or file specified it does not exist. {args.d}")
     else:
         if Path(args.encryption_file).exists():
-            encrypt_file_algo(args.file, args.encryption_file)
+            encrypt_file(args.file, args.encryption_file)
         else:
             print(f'did not specify encryption file or it does not exist. {args.e}')
